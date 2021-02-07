230ccdc3799a9002fafd8fe7a7b0a46a91b2837d
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2021-02-07 20:21:12 +0300

    поменял проект

---
+++ conftest.py
@@ -0,0 +1,112 @@
+from fixture.application import Application
+import pytest
+import json
+import importlib
+import os.path
+import jsonpickle
+from fixture.db import DbFixture
+import ftputil
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+@pytest.fixture
+def app(request, config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = config['web']
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config/config_inc.php.bak"):
+            remote.remove("config/config_inc.php.bak")
+        if remote.path.isfile("config/config_inc.php"):
+            remote.rename("config/config_inc.php", "config/config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config/config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config/config_inc.php.bak"):
+            if remote.path.isfile("config/config_inc.php"):
+                remote.remove("config/config_inc.php")
+            remote.rename("config/config_inc.php.bak", "config/config_inc.php")
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
+
---
+++ test/test_add_project.py
@@ -0,0 +1,19 @@
+from model.project import Project
+
+
+def test_add_project(app, json_project, check_ui, config):
+    project = json_project
+    old_projects = app.soap.get_project_list(username=config['web']["username"], password=config['web']["password"])
+    app.project.create(project)
+    new_projects = app.soap.get_project_list(username=config['web']["username"], password=config['web']["password"])
+    old_projects.append(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+    if check_ui:
+        assert sorted(new_projects, key=Project.id_or_max) == sorted(app.project.get_project_list(), key=Project.id_or_max)
+
+    def clean(cleaning_project):
+        return Project(identifier=cleaning_project.identifier, name=cleaning_project.name.strip(),
+                       description=cleaning_project.description.strip())
+    if check_ui:
+        clean_new_projects = map(clean, new_projects)
+        assert sorted(clean_new_projects, key=Project.id_or_max) == sorted(app.project.get_project_list(), key=Project.id_or_max)
