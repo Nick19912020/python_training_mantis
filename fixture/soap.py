070705deabc32057725ae351d54e8b153a9baf67
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2021-02-08 15:47:03 +0300

    задание 26_1

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,19 +9,16 @@ class SoapHelper:
     def __init__(self, app):
         self.app = app
 
-    def ensure_user_exists(self, username, password):
-        soap_config = self.app.config['soap']
-        session = SoapHelper.Session(
-            soap_config['host'], soap_config['port'], soap_config['username'], soap_config['password'])
-        if session.is_users_registered(username):
-            session.reset_password(username, password)
-        else:
-            session.create_user(username, password)
-        session.quit()
+    def can_login(self, username, password):
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
+        try:
+            client.service.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
 
     def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt-2.24.4/api/soap/mantisconnect.php?wsdl")
-
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         def convert(project):
             return Project(identifier=str(project.id), name=project.name, description=project.description)
         try:
